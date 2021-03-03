de24c85399558a94f7634c8f9b3357c1e65ee642
Tobi Jegede <toj@andrew.cmu.edu>
2021-03-03 00:12:34 -0500

    Loaded in datasets & added a header title, sidebar tabs, and placeholders for input and value boxes

--- hw2_toj/app.R
+++ hw2_toj/app.R
@@ -6,12 +6,56 @@ library(tidyverse)
 library(plotly)
 
 
-ui <- dashboardPage(
-    dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody()
+
+#Loading in the relevant datasets ---------------------------------
+
+#Covid Vaccination data by state
+covid.vac <- read.csv("covidvaccines.csv", header = TRUE)
+
+#Pfizer Vaccine Allocation data
+pf.alloc <- read.csv("pfizervaccineallocation.csv")
+
+
+header <- dashboardHeader(title = "COVID-19 Vaccine Distribution Tracker"
+  
+      
+    
+      
+    )
+  
+sidebar <-   dashboardSidebar(
+        sidebarMenu(
+        id = "tabs",
+  
+             # Menu Items ----------------------------------------------
+              menuItem("Overview of Vaccine Allocation", icon = icon("viruses"), tabName = "overview"),
+              menuItem("Vaccine Delivery", icon = icon("truck"), tabName = "delivery"),
+              menuItem("Vaccine Administration ", icon = icon("syringe"), tabName = "admin")
+        )
 )
 
-server <- function(input, output) { }
+
+body <- dashboardBody(tabItems(
+  
+  # Plot page ----------------------------------------------
+  tabItem("overview",
+      
+          #For the supplied state, display the % of vaccines delivered that are used
+  
+          # Input and Value Boxes ----------------------------------------------
+          fluidRow(
+            infoBoxOutput("Percent of Vaccine Used")
+            #valueBoxOutput()
+          )
+        )
+  )
+)
+
+
+ui <- dashboardPage(header, sidebar, body)
+
+server <- function(input, output) { 
+  
+  }
 
 shinyApp(ui, server)
