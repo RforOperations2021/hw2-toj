899a20c1ea621060a5bc264b32fc19d0aeb2ec60
Tobi Jegede <toj@andrew.cmu.edu>
2021-03-03 01:43:11 -0500

    Filled in info box and added a reactive data table

--- hw2_toj/app.R
+++ hw2_toj/app.R
@@ -4,6 +4,7 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(plotly)
+library(DT)
 
 
 
@@ -13,7 +14,10 @@ library(plotly)
 covid.vac <- read.csv("covidvaccines.csv", header = TRUE)
 
 #Pfizer Vaccine Allocation data
-pf.alloc <- read.csv("pfizervaccineallocation.csv")
+pf.alloc <- read.csv("pfizervaccineallocation.csv") 
+
+#Editing the column names of the pf.alloc dataset
+colnames(pf.alloc) <- c("Jurisdiction", "Week.of.Allocations", "First.Dose.Allocations", "Second.Dose.Allocations")
 
 
 header <- dashboardHeader(title = "COVID-19 Vaccine Distribution Tracker"
@@ -42,7 +46,7 @@ body <- dashboardBody(tabItems(
           #Selects the state to find information about (to be used to filter the data)
           selectInput(inputId = "state",
                       label = "Select a state",
-                      choices = covid.vac$State.Territory.Federal.Entity),
+                      choices = pf.alloc$Jurisdiction),
           
           
           #For the supplied state, display the % of vaccines delivered that are used
@@ -51,7 +55,11 @@ body <- dashboardBody(tabItems(
           fluidRow(
             infoBoxOutput("allocation")
             #valueBoxOutput()
-          )
+           ),
+          
+        dataTableOutput(outputId = "state_table")
+          
+          
         ),
   
   # Vaccine Delivery Page ----------------------------------------------
@@ -80,11 +88,23 @@ ui <- dashboardPage(header, sidebar, body)
 
 server <- function(input, output) { 
   
+  #Create a subset of the data filtering for state ---------------------------
+  state_alloc <- reactive({
+    req(input$state)
+    filter(pf.alloc, Jurisdiction %in% input$state)
+  })
   
    output$allocation <-
     renderInfoBox({
-      infoBox("Amount of Vaccine Allocated", value = 9000)
+      
+      infoBox("Amount of Vaccine Allocated", value = nrow(state_alloc()))
     })
+   
+   #Display data table of vaccines allocation for the selected state-------------------------------------------
+   output$state_table <- DT::renderDataTable({
+     DT::datatable(data = state_alloc(),
+                   rownames = FALSE)
+   })
   }
 
 shinyApp(ui, server)
